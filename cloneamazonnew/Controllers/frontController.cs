991d960286067f64e58ebcb8d6c48e1987da1006
Sentient Systems <sentientsystems@outlook.com>
2022-12-19 09:51:52 +0530

    Add project files.

---
+++ cloneamazonnew/Controllers/frontController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cloneamazonnew.Controllers
+{
+    public class frontController : Controller
+    {
+        // GET: front
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+    }
+}
---
+++ cloneamazonnew/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cloneamazonnew.MvcApplication" Language="C#" %>
